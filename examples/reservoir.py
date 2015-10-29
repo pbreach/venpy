77b7afc59a227a70a91a170bc184b3d2045f3a55
pbreach <pbreach@uwo.ca>
2015-10-28 21:27:03 -0400

    Changed 'tests' folder to 'examples'

--- examples/reservoir.py
+++ examples/reservoir.py
@@ -1,20 +1,15 @@
-import os
-
-os.chdir("..")
-
 from scipy.interpolate import interp2d
 import numpy as np
 
-import venpy as vp
+import venpy
 
-model = vp.load("tests/reservoir.vpm")
+model = venpy.load("reservoir.vpm")
 
 gate = np.arange(5)
 reservoir = np.arange(30)
 outflow = np.random.randn(30, 5)
 
 table = interp2d(gate, reservoir, outflow)
-
 
 def func():
     g = model['Gate Position']
@@ -24,6 +19,8 @@ def func():
 
 model['Outflow'] = func
 
-model.run()
+model.run(runname='Test')
+
+result = model.result()
 
 model.close()
